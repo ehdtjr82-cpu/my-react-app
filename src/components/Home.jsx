139edc87d35b59ec16ad92ae97dcdbfdf5cc2c53
sds <ehdtjr83@nate.com>
2026-05-10 10:05:12 +0900

    첫 리액트 프로젝트 업로드

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div style={{padding}}>
+            <h2>🏠 홈 대시보드</h2>
+            <p>시스템 현황을 한눈에 확인하는 메인 화면입니다.</p>
+        </div>
+    );
+};
